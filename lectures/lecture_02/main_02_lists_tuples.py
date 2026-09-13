799732bf683d15cf5f6cb709af32b50efe6f441b
Shawn Chahal <shawn.chahal@gmail.com>
2026-09-13 13:30:58 -0400

    Updated lecture_02 > main_2-3

--- lectures/lecture_02/main_02_lists_tuples.py
+++ lectures/lecture_02/main_02_lists_tuples.py
@@ -20,9 +20,9 @@ print(f"  {z[-2] = }")  # Access the 2nd last value in the list.
 print()
 
 print("You can access a slice of a list by various methods:")
-print(f"  {z[2:4] = }")  # Select indices 2 to 4, without including 4
-print(f"  {z[1:10:2] = }")  # Select indices 1 to 10, without including 10, in steps of 2.
-print(f"  {z[::-1] = }")  # Select all indices, in steps of -1. This reverses the list.
+print(f"  {z[2:4] = }")  # Start at idx=2, stop before idx=4.
+print(f"  {z[1:10:2] = }")  # Start at idx=1, stop before idx=10, in steps of 2.
+print(f"  {z[::-1] = }")  # All indices, in steps of -1. This reverses the list.
 print()
 
 print("You can get the length of a list using the len function:")
@@ -45,7 +45,8 @@ z.extend(a)
 print(f"  {z = }")
 print()
 
-print("You can make a 2D list by simply creating a \"list of lists\". The same indexing rules apply:")
+print("You can make a 2D list by simply creating a \"list of lists\".\n"
+      "The same indexing rules apply:")
 b = [
     [0, 1, 2],
     [3, 4, 5],
@@ -64,18 +65,29 @@ print(f"In the list at index 1, get the value at index 2:")
 print(f"  {b[1][2] = }")
 print()
 
-print("Note the difference between assignment and copying a list.")
+print("Note the difference between assignment and copying a list:")
 u = [1, 2, 3]
 v = u
 w = u.copy()
-print(f"  u = {u}")
-print(f"  v = {v}  (v does NOT create a new list. v and u both refer to the same list in memory.)")
-print(f"  w = {w}  (w creates a new list. w and u are different lists with the same contents.)")
+print(f"  {u = }")
+print(f"  {v = }  (v and u both refer to the same list in memory.)")
+print(f"  {w = }  (w and u are different lists with the same contents.)")
 print()
 
 print("Now if you modify the original list:")
 u[0] = 9
-print(f"  u = {u}")
-print(f"  v = {v}  (changed because v and u refer to the same list)")
-print(f"  w = {w}  (unchanged because w and u are different lists)")
+print(f"  {u = }")
+print(f"  {v = }  (changed because v and u refer to the same list)")
+print(f"  {w = }  (unchanged because w and u are different lists)")
+print()
+
+print("A tuple is a type of immutable list, meaning it is a list that \n"
+      "can't be modified:")
+x_list = [0, 1, 2, 3, 4]
+x_tuple = (0, 1, 2, 3, 4)
+print(f"  {x_list = }")
+print(f"  {x_tuple = }")
+print()
+
+print(f"Try modifying the tuple as you would a list and see what happens...")
 print()
--- lectures/lecture_02/main_03_for_loops.py
+++ lectures/lecture_02/main_03_for_loops.py
@@ -1,16 +1,25 @@
-# You can use a for loop to iterate over a range of numbers
+print()
+
+print("You can use a for loop to iterate over a range of numbers:")
 for i in range(10):  # Iterate 10 times ranging from 0 to 9
-    print(f"{i = }")
+    print(f"  {i = }")
 
 print()
 
-# You can also iterate over elements in a list directly
+print("You can iterate over elements in a list directly:")
 process_types = ["Batch", "Fed-Batch", "Continuous"]
 for process_type in process_types:
-    print(process_type)
+    print(f"  * {process_type}")
 
 print()
 
-# You can also keep track of a number as you iterate over a list
+print("You can keep track of a number as you iterate over a list:")
 for i, process_type in enumerate(process_types, start=1):
-    print(f"{i}) {process_type}")
+    print(f"  {i}) {process_type}")
+
+print()
+
+print("You can use a list comprehension to automate list construction:")
+x = [i ** 2 for i in range(10)]  # Make a list with the first 10 squares
+print(f"  {x = }")
+print()
